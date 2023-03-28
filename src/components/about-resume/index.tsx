3db0b767a34256f5d7c368a0da3036c1cd5dd3a8
Jose-WDG <neto.silvacem@gmail.com>
2023-03-27 21:50:36 -0300

    Removendo erros

--- src/components/about-resume/index.tsx
+++ src/components/about-resume/index.tsx
@@ -1,13 +1,13 @@
-import { AboutResumContainer, ImgJose } from "./style";
+import { AboutResumContainer } from "./style";
 
 export default function AboutResumSection() {
   return (
-      <AboutResumContainer className='about-resum' id="about-resum">
-        <div className="about-container">
-          <h1 className='name-title'>José Ramalho da silva neto</h1>
-          <h2 className='office-title'>Desenvolvedor Mobile</h2>
-          <h3 className='training'>Programador e desenvolvedor há mais de 3 anos atuando diretamente no mercado.</h3>
-        </div>
-      </AboutResumContainer>
+    <AboutResumContainer className='about-resum' id="about-resum">
+      <div className="about-container">
+        <h1 className='name-title'>José Ramalho da silva neto</h1>
+        <h2 className='office-title'>Desenvolvedor Mobile</h2>
+        <h3 className='training'>Programador e desenvolvedor há mais de 3 anos atuando diretamente no mercado.</h3>
+      </div>
+    </AboutResumContainer>
   )
 }
--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -2,29 +2,29 @@ import { ContactContainer } from "./stylex";
 
 export default function ContactSection() {
 	return (
-			<ContactContainer id="contact">
-				<h2 className="sobre-txt">Contato</h2>
-				<ul className="informacoes">
-					<li>WhatsApp: 11 9 92339592</li>
-					<li>E-mail: neto.silva101@outlook.com</li>
-				</ul>
-				<ul className="icons">
-					<li>
-						<a href="https://www.linkedin.com/in/jos%C3%A9-ramalho-8a1a07159/">
-							<span className="label"><i class="fa fa-linkedin-square" aria-hidden="true"></i>
- LinkedIn</span>
-						</a>
-					</li>
-					<li>
-						<a href="https://github.com/Jose-WDG">
-							<span className="label"><i className="fa fa-github" aria-hidden="true"></i> GitHub</span>
-						</a>
-					</li>
-				</ul>
-				<ul className="copyright">
-					<li>&copy; José Ramalho. Todos os direitos reservados.</li>
-					<li>Design: José Ramalho</li>
-				</ul>
-			</ContactContainer>
+		<ContactContainer id="contact">
+			<h2 className="sobre-txt">Contato</h2>
+			<ul className="informacoes">
+				<li>WhatsApp: 11 9 92339592</li>
+				<li>E-mail: neto.silva101@outlook.com</li>
+			</ul>
+			<ul className="icons">
+				<li>
+					<a href="https://www.linkedin.com/in/jos%C3%A9-ramalho-8a1a07159/">
+						<span className="label"><i className="fa fa-linkedin-square" aria-hidden="true"></i>
+							LinkedIn</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://github.com/Jose-WDG">
+						<span className="label"><i className="fa fa-github" aria-hidden="true"></i> GitHub</span>
+					</a>
+				</li>
+			</ul>
+			<ul className="copyright">
+				<li>&copy; José Ramalho. Todos os direitos reservados.</li>
+				<li>Design: José Ramalho</li>
+			</ul>
+		</ContactContainer>
 	)
 }
